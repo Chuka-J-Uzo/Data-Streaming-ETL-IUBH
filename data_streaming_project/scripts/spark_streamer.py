f0a067b8346a319878bcaf4813f05a02c9077c5e
Chuka Uzo <95084188+Chuka-J-Uzo@users.noreply.github.com>
2023-02-09 11:23:15 +0000

    commit with first spark code and new spark dbs

--- data_streaming_project/scripts/spark_streamer.py
+++ data_streaming_project/scripts/spark_streamer.py
@@ -43,28 +43,64 @@ df = spark \
     .option('schema', schema) \
     .load()
 
-# Write the data to a Postgres table
-df.writeStream \
-    .format('jdbc') \
-    .option('url', 'jdbc:postgresql://localhost:5432/test_db1') \
-    .option('dbtable', 'table_1') \
-    .option('user', 'root') \
-    .option('password', 'root') \
-    .option('checkpointLocation', '/tmp/checkpoints') \
-    .start()
 
-# Perform any transformations or aggregations on the data as needed
-df_transformed = df.select(...)
+# Now, let's perform transformations on the inbound data as needed
+# 1. Calculate the average pitch for each second
+df_transformed = df.groupBy(df['seconds']).agg(avg(df['pitch']).alias("avg_pitch"))
 
-# Write the transformed data to another Postgres table
+# 2. Filter data with only specific columns and rename the columns
+df_transformed = df.select("time", "qz", "qy", "qx", "qw") \
+                   .withColumnRenamed("time", "timestamp") \
+                   .withColumnRenamed("qz", "z_axis") \
+                   .withColumnRenamed("qy", "y_axis") \
+                   .withColumnRenamed("qx", "x_axis") \
+                   .withColumnRenamed("qw", "w_axis")
+
+# 3. Group the data by "timestamp" and compute the average of each axis
+df_grouped = df_transformed.groupBy(df_transformed.timestamp) \
+                           .agg(avg(df_transformed.z_axis),
+                                avg(df_transformed.y_axis),
+                                avg(df_transformed.x_axis),
+                                avg(df_transformed.w_axis))
+
+
+
+# Define the schema for the transformed data
+schema_transformed = StructType([
+    StructField("timestamp", TimestampType(), True),
+    StructField("seconds", IntegerType(), True),
+    StructField("avg_qz", DoubleType(), True),
+    StructField("avg_qy", DoubleType(), True),
+    StructField("avg_qx", DoubleType(), True),
+    StructField("avg_qw", DoubleType(), True),
+    StructField("avg_roll", DoubleType(), True),
+    StructField("avg_pitch", DoubleType(), True),
+    StructField("avg_yaw", DoubleType(), True)
+])
+
+
+# Perform transformations on the data
+df_transformed = df.withColumn("timestamp", from_unixtime(col("time")/1000, "yyyy-MM-dd HH:mm:ss")) \
+                   .groupBy(window("timestamp", "1 minute")) \
+                   .agg(avg("qz").alias("avg_qz"),
+                        avg("qy").alias("avg_qy"),
+                        avg("qx").alias("avg_qx"),
+                        avg("qw").alias("avg_qw"),
+                        avg("roll").alias("avg_roll"),
+                        avg("pitch").alias("avg_pitch"),
+                        avg("yaw").alias("avg_yaw"))
+
+
+# Now, we write our transformed data to a MySQL table
 df_transformed.writeStream \
-    .format('jdbc') \
-    .option('url', 'jdbc:postgresql://localhost:5432/test_db1') \
-    .option('dbtable', 'table_1_transformed') \
-    .option('user', 'root') \
-    .option('password', 'root') \
-    .option('checkpointLocation', '/tmp/checkpoints') \
+    .format("jdbc") \
+    .option("url", "jdbc:mysql://localhost:3306/SPARK_TRANSFORMED_DB") \
+    .option("dbtable", "SPARK_TABLE_TRANSFORMED") \
+    .option("user", "root") \
+    .option("password", "root") \
+    .option("checkpointLocation", "/tmp/checkpoints") \
     .start()
+
 
 
 # Submit my file to spark-submit
