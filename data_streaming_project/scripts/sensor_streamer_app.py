ec9fe38f76017f798b20afcd8083c744c8798e1b
Chuka-J-Uzo <chukauzo.j@gmail.com>
2023-03-11 18:11:31 +0100

    deduplicate function implemented. Finally, each code continues from where stoppedgit add .

--- data_streaming_project/scripts/sensor_streamer_app.py
+++ data_streaming_project/scripts/sensor_streamer_app.py
@@ -6,6 +6,7 @@ import socket
 import sqlalchemy
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, DateTime
 from sqlalchemy.sql import insert
+from sqlalchemy.exc import IntegrityError
 from confluent_kafka import Producer
 
 # setup database connection
@@ -21,10 +22,12 @@ connection = engine.connect()
 metadata = MetaData()
 
 truck_data = Table('TRUCK_PARAMETER_MAP', metadata,
+                   Column('id', Integer, primary_key=True),
                    Column('timestamp', DateTime),
                    Column('distance_covered', Float),
                    Column('engine_speed', Float),
                    Column('fuel_consumed', Float))
+
 
 metadata.create_all(engine)
 
@@ -45,20 +48,31 @@ def produce_truck_data():
         fuel_tank_capacity = 50
         fuel_remaining = fuel_tank_capacity
         engine_speed = 80
+        counter = 0
+        
+        # Get the latest timestamp in the database
+        latest_timestamp_query = truck_data.select().order_by(truck_data.c.timestamp.desc()).limit(1)
+        latest_timestamp = connection.execute(latest_timestamp_query).scalar()
+        
+        # If no records in the database yet, set the timestamp to the current time
+        if latest_timestamp is None:
+            latest_timestamp = datetime.now()
+        else:
+            latest_timestamp = pd.to_datetime(latest_timestamp)
+        
         for i in range(100):
             distance_travelled = i + 1
             fuel_consumed = (distance_travelled / distance_covered) * fuel_tank_capacity
             fuel_remaining -= fuel_consumed
             if fuel_remaining <= 0:
                 break
-            timestamp = datetime.now()
+            timestamp = latest_timestamp + pd.Timedelta(seconds=counter)
             data = {"timestamp": str(timestamp),
                     "distance_covered": np.round(distance_travelled, 2),
                     "engine_speed": np.round(engine_speed, 2),
                     "fuel_consumed": np.round(fuel_consumed, 2)}
             producer.produce("Truck-Data", value=str(data).encode())
-
-
+            
             # Insert generated data into mysql database
             stmt = insert(truck_data).values(
                 timestamp=timestamp, 
@@ -66,10 +80,21 @@ def produce_truck_data():
                 engine_speed=data['engine_speed'], 
                 fuel_consumed=data['fuel_consumed']
             )
-            connection.execute(stmt)
+            
+            try:
+                connection.execute(stmt)
+            except IntegrityError:
+                # If the insert fails due to a duplicate key error, update the existing record instead
+                stmt = truck_data.update().values(
+                    distance_covered=data['distance_covered'],
+                    engine_speed=data['engine_speed'], 
+                    fuel_consumed=data['fuel_consumed']
+                ).where(truck_data.c.timestamp == timestamp)
+                connection.execute(stmt)
+            
             connection.commit()
-
-                
+            
+            counter += 1
             print("Message sent successfully")
 
         producer.flush() # flushes all outstanding messages from the buffer.
@@ -78,7 +103,8 @@ def produce_truck_data():
         print("Exception in sending message")
         print(str(e))
 
-
+    finally:
+        connection.close()
 
 if __name__ == '__main__':
-    produce_truck_data()   
+    produce_truck_data()
--- data_streaming_project/scripts/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Load our dataset into a Pandas Dataframe for advanced manipulations.
-df = pd.read_csv(r"data_streaming_project/datasets/Orientation.csv")
-# Convert our first column which is in UNIX EPOCH time to DMY....a format that Apache Superset will pick easily.
-df['time'] = pd.to_datetime(df['time'], unit='ms').dt.strftime('%d-%m-%Y %H:%M:%S')
-print(df.head(5))
